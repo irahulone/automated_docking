a1028b31a031e27bd54e6eccce645c3bfa234e72
manoj sharma <imanoj_sharma@yahoo.com>
2025-04-23 00:00:25 -0700

    Update setup.py

--- control_directives/setup.py
+++ control_directives/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'director = control_directives.docking_director:main',
             'fcmc = control_directives.fluid_charge_management_controller:main',
         ],
     },
